a973940fb360455ca8c5a6551a924474ea689495
BernardMasache <esc-64-15@cc.ac.mw>
2021-07-23 10:30:29 -0700

    working on adding post interface and select image to be uploaded on firebase

---
+++ app/src/main/java/com/example/assignment/ActivityFragments.java
@@ -0,0 +1,142 @@
+package com.example.assignment;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class ActivityFragments extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter adapter;
+    FloatingActionButton add_notice;
+    private Button import_file_id;
+    private View dialogView;
+    private int resultCode =1;
+    private NumberPicker duration, file_type, notification_type;
+    private String[] durations = {"Day", "Week", "Month"};
+    private String[] file_types = {"jpg", "png", "pdf", "docx"};
+    private String[] notification_types = {"Academic", "Religion", "Sports", "Business", "AOBs"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragments);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        dialogView = inflater.inflate(R.layout.advertpost, null);
+        duration = (NumberPicker) dialogView.findViewById(R.id.post_duration);
+        file_type = (NumberPicker) dialogView.findViewById(R.id.post_file_type);
+        notification_type = (NumberPicker) dialogView.findViewById(R.id.post_type);
+        import_file_id = (Button) dialogView.findViewById(R.id.import_file_id);
+
+        add_notice = (FloatingActionButton) findViewById(R.id.add_notice);
+
+        add_notice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getValuesNotes();
+
+            }
+        });
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout_id);
+        viewPager2 = (ViewPager2) findViewById(R.id.viewpager_id);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fragmentManager, getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Academics"));
+        tabLayout.addTab(tabLayout.newTab().setText("Religion"));
+        tabLayout.addTab(tabLayout.newTab().setText("Business"));
+        tabLayout.addTab(tabLayout.newTab().setText("Sports"));
+        tabLayout.addTab(tabLayout.newTab().setText("AOBs"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void getValuesNotes(){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+        duration.setMaxValue(2);
+        duration.setMinValue(0);
+        duration.setDisplayedValues(durations);
+
+        file_type.setMaxValue(3);
+        file_type.setMinValue(0);
+        file_type.setDisplayedValues(file_types);
+
+        notification_type.setMaxValue(4);
+        notification_type.setMinValue(0);
+        notification_type.setDisplayedValues(notification_types);
+
+        dialogBuilder.setView(dialogView);
+        getImageDetails();
+        dialogBuilder.setTitle("Add your notification.");
+        dialogBuilder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Saved successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialogBuilder.setNegativeButton("Cancel", null);
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
+    public void getImageDetails(){
+        import_file_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent imageIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                imageIntent.setType("image/*");
+                startActivityForResult(imageIntent, resultCode);
+            }
+        });
+    }
+}
